717a4d2e5e459017e997608e6572b87c9d029485
André Carvalho <afecarvalho@gmail.com>
2022-02-26 18:39:28 +0000

    Fix package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="repo_b",
+    name="repo_c",
     version="0.0.1",
     install_requires=["pymongo", "jsonschema"],
     packages=find_packages("."),
